5ae5c686ec6047a6723ea7b77dfa48d9d167e012
ducmanht6-hub <ducmanht6@gmail.com>
2026-04-20 21:35:56 +0700

    Update Bai 1.sql

--- Bai 1.sql
+++ Bai 1.sql
@@ -8,19 +8,15 @@
 -- OR (district = 'Quận 3' AND rating > 4.0);
 
 -- Hệ quả:
-
 -- Tất cả nhà hàng ở Quận 1 đều được lấy ra (không cần biết rating bao nhiêu).
 -- Chỉ Quận 3 mới bị áp điều kiện rating > 4.0.
 -- Đó chính là lý do Marketing thấy các quán rating thấp ở Quận 1 vẫn xuất hiện.
 
--- Cách sửa (chuẩn xác)
--- Bạn cần gom điều kiện khu vực lại trước bằng dấu ngoặc:
+-- Cách sửa 
 SELECT restaurant_name, address, rating
 FROM Restaurants
 WHERE (district = 'Quận 1' OR district = 'Quận 3')
   AND rating > 4.0;
-
---  Lúc này logic sẽ đúng:
 
 -- Chỉ lấy nhà hàng thuộc Quận 1 hoặc Quận 3
 -- Và đồng thời phải có rating > 4.0
